c1a47df239293933cbeb91c0497b094a618bd748
ElRay32 <guerrero20191@outlook.es>
2025-08-15 10:30:20 -0400

    Class Library Interfaces
    Interfaces:
    -IClubService

---
+++ LectoTribu.Application/Interfaces/IClubService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LectoTribu.Application.DTOs;
+using LectoTribu.Domain.Entities;
+
+namespace LectoTribu.Application.Interfaces;
+
+public interface IClubService
+{
+    Task<Club> CreateClubAsync(CreateClubDto dto, CancellationToken ct = default);
+    Task AddBookAsync(AddBookDto dto, CancellationToken ct = default);
+    Task AddMemberAsync(Guid clubId, Guid userId, CancellationToken ct = default);                 // overload 1
+    Task AddMemberAsync(AddMemberByEmailDto dto, CancellationToken ct = default);                  // overload 2
+    Task ScheduleAsync(ScheduleChapterDto dto, CancellationToken ct = default);                    // overload 1
+    Task ScheduleAsync(SchedulePlanDto dto, CancellationToken ct = default);                       // overload 2
+    Task CommentAsync(CommentDto dto, CancellationToken ct = default);
+}
--- LectoTribu.Application/obj/Debug/net8.0/LectoTribu.Application.AssemblyInfo.cs
+++ LectoTribu.Application/obj/Debug/net8.0/LectoTribu.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LectoTribu.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba95d31770aae5be7711b9b78bc7c1719511c95f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6c53401b51f1dd0d96cf01b821482be2b55962")]
 [assembly: System.Reflection.AssemblyProductAttribute("LectoTribu.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LectoTribu.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
