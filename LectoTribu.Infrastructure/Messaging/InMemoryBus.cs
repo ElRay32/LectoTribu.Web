c585e68755c26c78de8752d2723b795d65145661
ElRay32 <guerrero20191@outlook.es>
2025-08-15 14:12:08 -0400

    Class Library Messaging
    Messaging:
    -InMemoryBus

---
+++ LectoTribu.Infrastructure/Messaging/InMemoryBus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using LectoTribu.Domain.Abstractions;
+
+namespace LectoTribu.Infrastructure.Messaging;
+
+public class InMemoryBus : IMessageBus
+{
+    private readonly ConcurrentDictionary<Type, List<Func<object, Task>>> _handlers = new();
+
+    public Task PublishAsync<T>(T message, CancellationToken ct = default)
+    {
+        if (_handlers.TryGetValue(typeof(T), out var typed))
+            return Task.WhenAll(typed.Select(h => h(message!)));
+        if (message != null && _handlers.TryGetValue(message.GetType(), out var dyn))
+            return Task.WhenAll(dyn.Select(h => h(message!)));
+        return Task.CompletedTask;
+    }
+
+    public void Subscribe<T>(Func<T, Task> handler)
+    {
+        var list = _handlers.GetOrAdd(typeof(T), _ => new());
+        list.Add(msg => handler((T)msg));
+    }
+}
